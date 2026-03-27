1536bbef9f7eaf57c53b41cb2f0235b4384521f8
timeo-grrd <timeog53@gmail.com>
2026-03-27 10:30:59 +0100

    Ajout quotidien : Variables.py

---
+++ Variables.py
@@ -0,0 +1,57 @@
+#le début de la fin
+
+#strings
+hello= "bonjour " 
+food="pizza"
+email="tim@caca.com"
+# print(F"salam {hello}")
+# print(f"hello you like {food}")
+# print(f"my email is {email}")
+
+
+#Integers
+age =56  
+quantité= 1.22
+nbr_etudiant=4212
+# print (f"j'ai {age} ans")
+# print (f"j'ai acheté {quantité} bananananne")
+# print(f"il y a {nbr_etudiant} in paris")
+
+#Float 
+price=42.2
+gpa = 25.226662
+distance= 15.1225
+# print(f"le prix est de {price}€")
+# print(f"mon gpa est de {gpa}")
+# print(f"le prochain mc do est à {distance}km")
+
+
+#boolean
+for_sale = False
+is_student = False
+is_online = False
+# if is_student:
+#     print("you are a student")
+# else:
+#     print("you are not student")
+
+# if for_sale:
+#     print("that item is for sale")
+# else:
+#     print("that item is not available")
+
+# if is_online :
+#     print("le joueur est en ligne")
+# else:
+#     print ("le joueur est inactif")
+
+user_name= "Tim" 
+annee=2026
+pi=3.14
+is_admin =False
+
+print(f"je m'appelle {user_name} et nous sommes en {annee}, le nombre pi est {pi}")
+if is_admin:
+    print ("je suis co")
+else:
+    print("je ne suis pas co")
