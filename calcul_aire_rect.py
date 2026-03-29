32ca93ea2278f957597d10c380398c89ca23b892
timeo-grrd <timeog53@gmail.com>
2026-03-29 11:41:13 +0200

    Ajout quotidien : calcul_aire_rect.py

---
+++ calcul_aire_rect.py
@@ -0,0 +1,4 @@
+longueur = float(input("veuillez rentrez une longueur en cm ") )
+largeur = float(input("veuillez rentrez une largeur cm "))
+aire= largeur *longueur
+print(f"L'aire du rectangle est de {aire} cm²")
