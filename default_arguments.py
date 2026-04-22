92ab71bba3bb340cfa7ba55c650f7296802dea75
timeo-grrd <timeog53@gmail.com>
2026-04-22 16:58:25 +0200

    Ajout quotidien : default_arguments.py

---
+++ default_arguments.py
@@ -0,0 +1,15 @@
+# def net_price(list_price, discount=0, tax=0.05):
+#     return list_price * (1 - discount) * (1 + tax)
+
+# print(net_price(500))
+# print(net_price(500, 0.1))
+# print(net_price(500, 0.1, 0))
+
+import time 
+def count (end, start=0):
+    for x in range(start, end+1):
+        print(x)
+        time.sleep(1)
+    print("done")
+
+count (10)
