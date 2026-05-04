fa7c004771df550c7e7f4ba86b6c5b139855cb65
timeo-grrd <timeog53@gmail.com>
2026-05-04 09:10:02 +0200

    Ajout quotidien : PyQt5_GUI_intro.py

---
+++ PyQt5_GUI_intro.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My cool first GUI")
+        self.setGeometry(700, 300, 500, 500)
+        self.setWindowIcon(QIcon("astraunote(9).jpg"))
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
